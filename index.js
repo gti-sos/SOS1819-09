042451129b66153c0218239cd3f79d07c2611fe6
sos1819gw <gauthier.wiemann@insa-rouen.fr>
2019-03-26 12:02:50 +0000

    deletion of an useless line

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-var paginate = require('express-paginate');
 
 var app = express();
 var port = process.env.PORT || 8080;
