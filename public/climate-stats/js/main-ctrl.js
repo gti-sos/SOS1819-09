6f5d818f09d577831546b78c5de1c21b13ec039b
sos1819gw <gauthier.wiemann@insa-rouen.fr>
2019-04-08 16:25:56 +0000

    F06 done

--- public/climate-stats/js/main-ctrl.js
+++ public/climate-stats/js/main-ctrl.js
@@ -6,10 +6,51 @@ app.controller("MainCtrl", ["$scope","$http", function ($scope,$http){
                 console.log("Modular MainCtrl initialized");
                 $scope.url = "/api/v1/climate-stats";
                 
+                // GET
                 
                 $scope.send = function(){
                     $http.get($scope.url).then(function (response){
+                        $scope.status = response.status;
                         $scope.data = JSON.stringify(response.data,null,2);
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+                
+                // POST
+                
+                $scope.post = function(){
+                    $http.post($scope.url,$scope.body).then(function (response){
+                        $scope.status = response.status;
+                        $scope.data = "";
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+                
+                // PUT
+                
+                $scope.put = function(){
+                    $http.put($scope.url,$scope.body).then(function (response){
+                        $scope.status = response.status;
+                        $scope.data = "";
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
+                    });
+                };
+                
+                // DELETE
+                
+                $scope.delete = function(){
+                    $http.delete($scope.url).then(function(response){
+                        $scope.status = response.status;
+                        $scope.data = "";
+                    }, function (error){
+                        $scope.status = error.status;
+                        $scope.data = "";
                     });
                 };
 }]);
