ca6c14d193cf6d19e3afdfec1cfdb73b100eae9c
sos1819gw <gauthier.wiemann@insa-rouen.fr>
2019-04-22 14:37:57 +0000

    Tarea 1

--- public/climate-stats/js/main-ctrl.js
+++ public/climate-stats/js/main-ctrl.js
@@ -6,6 +6,85 @@ app.controller("MainCtrl", ["$scope","$http", function ($scope,$http){
                 console.log("Modular MainCtrl initialized");
                 $scope.url = "/api/v1/climate-stats";
                 
+                
+                refresh();
+                
+                function refresh(){
+                    $http.get($scope.url).then(function (response){
+                        $scope.climates = response.data;
+                    });
+                }
+                
+                function sleep(milliseconds) {
+                  var start = new Date().getTime();
+                  for (var i = 0; i < 1e7; i++) {
+                    if ((new Date().getTime() - start) > milliseconds){
+                      break;
+                    }
+                  }
+                }
+                
+                $scope.getFromTo = function(){
+                    console.log("buscando");
+                    $http({
+                        url: $scope.url, 
+                        method: "GET",
+                        params: {from: $scope.from, to: $scope.to}
+                     }).then(function (response){
+                        $scope.climates = response.data;
+                        console.log(JSON.stringify(response.data,null,2));
+                    });
+                };
+                
+                $scope.getCountry = function(){
+                    console.log("buscando");
+                    $http({
+                        url: $scope.url, 
+                        method: "GET",
+                        params: {country : $scope.country}
+                     }).then(function (response){
+                        $scope.climates = response.data;
+                        console.log(JSON.stringify(response.data,null,2));
+                    });
+                };
+
+                $scope.addClimate = function(){
+            
+                    console.log("Adding climate");
+                    $scope.newClimate.year = parseInt($scope.newClimate.year,10);
+                    $scope.newClimate.methane_stats = parseFloat($scope.newClimate.methane_stats);
+                    $scope.newClimate.co2_stats = parseFloat($scope.newClimate.co2_stats);
+                    $scope.newClimate.nitrous_oxide_stats = parseFloat($scope.newClimate.nitrous_oxide_stats);
+                    
+                    $http.post($scope.url,$scope.newClimate).then(function (response){
+                        console.log("Climate added");
+                        refresh();
+                    });
+                };
+                
+                $scope.deleteClimate = function(country, year){
+            
+                    console.log("Deleting climate");
+                    $http.delete($scope.url+"/"+country+"/"+year).then(function (response){
+                        console.log("Climate deleted");
+                        sleep(100); //Database update
+                        refresh();
+                    });
+                };
+                
+                // DELETE
+                
+                $scope.delete = function(){
+                    $http.delete($scope.url).then(function(response){
+                        $scope.status = response.status;
+                        sleep(100); //Database update
+                        refresh();
+                    }, function (error){
+                        $scope.status = error.status;
+                    });
+                };
+                
+                /*
                 // GET
                 
                 $scope.send = function(){
@@ -42,15 +121,5 @@ app.controller("MainCtrl", ["$scope","$http", function ($scope,$http){
                     });
                 };
                 
-                // DELETE
-                
-                $scope.delete = function(){
-                    $http.delete($scope.url).then(function(response){
-                        $scope.status = response.status;
-                        $scope.data = "";
-                    }, function (error){
-                        $scope.status = error.status;
-                        $scope.data = "";
-                    });
-                };
+                */
 }]);
