2a7982334bb1fd90815473801852e76076479583
sos1819eb <emma.bonhomme@gmail.com>
2019-05-12 16:13:29 +0000

    routage

---
+++ public/publicpopstatsapp/js/list-ctrl.js
@@ -0,0 +1,112 @@
+/* global angular*/
+
+angular
+
+    .module("PopApp")
+    
+    .controller("list-ctrl",["$scope", "$http", function($scope,$http){
+        
+        console.log("list ctrl initialized");
+        
+        var API = "/api/v1/populationstats";
+        
+        function refresh(){
+            $http.get(API).then(function(response){
+            
+                $scope.popstats = response.data;
+            });
+        }
+        
+        refresh();
+        
+        $scope.add = function(){
+            var newPopStat= $scope.newPopStat;
+            if (isNaN($scope.newPopStat.year)){
+                $scope.message = $scope.newPopStat.year + " no es un año, por favor añade un recurso con campos correctos"
+            } else {
+                $scope.newPopStat.year = parseInt(newPopStat.year);
+                
+                console.log("adding stats "+JSON.stringify(newPopStat,null,2));
+                
+                $http.post(API, newPopStat)
+                     .then(function(response){
+                    
+                    console.log("post response "+ response.statusText);
+                    
+                    $scope.message = response.statusText;
+                    
+                    $scope.newPopStat="";
+                    
+                    refresh();
+                })
+                    .catch(function(data){
+                        console.log(data.status);
+                        $scope.message = data.statusText+" : Añade un recurso con campos correctos";
+                    });
+            }
+        };
+        
+        $scope.delete = function(stat){
+            console.log("deleting stats <"+ stat.country +">");
+            $http.delete(API+"/"+stat.country+"/"+stat.year)
+                 .then(function(response){
+                     console.log("delete response"+ response.statusText);
+                     $scope.message = response.statusText;
+                     refresh();
+                     
+                 });
+        };
+        
+        $scope.deleteall = function (){
+            $http.delete(API)
+                 .then(function(response){
+                    console.log("delete all response"+ response.statusText);
+                    $scope.message = response.statusText;
+                    refresh();
+                 });
+        };
+        
+        $scope.ver = function () {
+            var year1=$scope.year1;
+            var year2=$scope.year2;
+            console.log("ver de <"+ year1 +"> a <" + year2+">");
+            console.log("<"+API+"?from="+year1+"&to="+year2+">");
+            $http.get(API+"?from="+year1+"&to="+year2)
+                 .then(function(response){
+                     
+                     $scope.popstats = response.data;
+                     $scope.message = response.statusText;
+                     //refresh();
+                 });
+        };
+        
+        $scope.vercountry = function () {
+            var country = $scope.inputcountry;
+            console.log("ver recurso : <"+ country + ">");
+            $http.get(API+"/"+country)
+                 .then(function(response){
+                     $scope.popstats = response.data;
+                     //refresh();
+                 })
+                 .catch(function(data){
+                        console.log(data.status);
+                        refresh();
+                        $scope.message = data.statusText+" : El recurso "+country+" no existe";
+                    });
+        };
+        
+        
+        $scope.guardarUpdate = function (updateStat) {
+            updateStat = $scope.updateStat;
+            console.log("PUT : "+API+"/"+updateStat.country+"/"+updateStat.year);
+            $http.put(API+"/"+updateStat.country+"/"+updateStat.year,updateStat)
+                 .then( function(response){
+                     console.log("Put response : "+response.status);
+                     $scope.updateStat = "";
+                     refresh();
+                    $scope.message = response.statusText;
+                    
+                });
+        };
+        
+    }]);
--- public/publicpopstatsapp/js/pop-app.js
+++ public/publicpopstatsapp/js/pop-app.js
@@ -1,3 +1,18 @@
 /*global angular*/
-angular.module("PopApp",[]);
+
+
+angular.module("PopApp",["ngRoute"])
+        .config(function($routeProvider){
+            $routeProvider
+                .when("/",{
+                    controller : "list-ctrl",
+                    templateUrl : "list.html"
+                })
+                .when("/edit/:country/:year",{
+                    controller :"edit-ctrl",
+                    templateUrl : "edit.html"
+                });
+        });
+
+
 console.log("Modular PopApp initialized");
