5d196b9a50f4c82502da6d19d7b3692e9c6a0e4a
sos1819gp <pimpinella.giuseppe@libero.it>
2019-05-14 21:43:59 +0000

    D02

--- testseconomy/tests.js
+++ testseconomy/tests.js
@@ -2,10 +2,9 @@ exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	chromeOnly: true,
 	specs: [
-		'e2e/TCP01-loadData.js',
-		'e2e/TCP02-newStat.js',
-		'e2e/TCP03-deleteStat.js'
-		
+		'e2e/TCP01-newEconomy.js',
+		'e2e/TCP02-loadEconomy.js',
+		'e2e/TCP03-deleteEconomy.js'
 	]
 
 };
