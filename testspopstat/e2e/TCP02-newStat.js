ca793f27582418dc15a589305bdfe976fd15c957
sos1819eb <emma.bonhomme@gmail.com>
2019-05-14 14:36:33 +0000

    D02 Emma part2

--- testspopstat/e2e/TCP02-newStat.js
+++ testspopstat/e2e/TCP02-newStat.js
@@ -4,21 +4,21 @@ describe('Check if a new popstat can be created', function () {
 		                     element
 		                        .all(by.repeater('stat in popstats'))
 		                        .then(function(intialStats){
-		                            
+
 		                            element(by.model('newPopStat.country')).sendKeys('France');
 		                            element(by.model('newPopStat.year')).sendKeys(2019);
 		                            element(by.model('newPopStat.totalpopulation')).sendKeys('66000000');
 		                            element(by.model('newPopStat.urbanpopulation')).sendKeys('60000000');
 		                            element(by.model('newPopStat.accesstoelectricity')).sendKeys('99');
-		                            
+
 		                            element(by.css('[value="add"]')).click();
-		                            
+
 		                            element
 		                                .all(by.repeater("stat in popstats"))
 		                                .then(function(finalStats){
-		                                    
+
 		                                    expect(finalStats.length).toEqual(intialStats.length+1);
-		                                    
+
 		                                });
 		                        });
 		//expect(stats.count()).toBeGreaterThan(0);
