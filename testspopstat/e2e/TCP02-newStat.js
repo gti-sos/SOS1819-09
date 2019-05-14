d857d9618661a21d0f27f6c077a3c2a35d9da089
sos1819eb <emma.bonhomme@gmail.com>
2019-05-14 16:04:28 +0000

    D02 again Emma

--- testspopstat/e2e/TCP02-newStat.js
+++ testspopstat/e2e/TCP02-newStat.js
@@ -1,26 +1,30 @@
 describe('Check if a new popstat can be created', function () {
 	it('The new stat should be showed', function (){
-		browser.get('http://localhost:8080/ui/v1/populationstats/');
+		browser.get('http://localhost:8080/#!/populationstats/');
 		                     element
 		                        .all(by.repeater('stat in popstats'))
-		                        .then(function(intialStats){
-
+		                        .then(function(initialStats){
+		                        	
+									console.log("new 1 : "+initialStats.length);
 		                            element(by.model('newPopStat.country')).sendKeys('France');
 		                            element(by.model('newPopStat.year')).sendKeys(2019);
 		                            element(by.model('newPopStat.totalpopulation')).sendKeys('66000000');
 		                            element(by.model('newPopStat.urbanpopulation')).sendKeys('60000000');
 		                            element(by.model('newPopStat.accesstoelectricity')).sendKeys('99');
-
+									
+									console.log("new 2: "+initialStats.length);
+									
 		                            element(by.css('[value="add"]')).click();
+
+									console.log("new 3: "+initialStats.length);
 
 		                            element
 		                                .all(by.repeater("stat in popstats"))
 		                                .then(function(finalStats){
-
-		                                    expect(finalStats.length).toEqual(intialStats.length+1);
-
+		                                	console.log("new 4: "+initialStats.length);
+		                                	console.log("new Final: "+finalStats.length);
+		                                    expect(finalStats.length).toEqual(initialStats.length);
 		                                });
 		                        });
-		//expect(stats.count()).toBeGreaterThan(0);
 	});
 });
