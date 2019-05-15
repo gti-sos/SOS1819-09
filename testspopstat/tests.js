078551ece8e17379a67397385a92dcb2cb981295
sos1819gw <gauthier.wiemann@insa-rouen.fr>
2019-05-15 08:27:57 +0000

    lol

--- testspopstat/tests.js
+++ testspopstat/tests.js
@@ -2,8 +2,8 @@ exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	chromeOnly: true,
 	specs: [
-		"e2e/TC01-newEconomy.js",
-    	"e2e/TC02-loadEconomy.js",
-    	"e2e/TC03-deleteEconomy.js"
+		"e2e/TCP01-loadData.js",
+    	"e2e/TCP02-newStat.js",
+    	"e2e/TCP03-deleteStat.js"
 	]
 };
