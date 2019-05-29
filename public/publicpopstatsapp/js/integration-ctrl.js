6c9218c35fb3dac010427e9f3c1e466ed6f452e3
sos1819eb <emma.bonhomme@gmail.com>
2019-05-29 12:24:06 +0000

    integration

--- public/publicpopstatsapp/js/integration-ctrl.js
+++ public/publicpopstatsapp/js/integration-ctrl.js
@@ -11,15 +11,64 @@ angular
                     "$location",
     function($scope,$http,$routeParams,$location){
         
-        console.log("chart ctrl initialized");
+        console.log("integration ctrl initialized");
         
-        //var API = "/api/v1/populationstats";
-        var API="https://sos1819-08.herokuapp.com/api/v1/emigrations-by-countries/";
+        var APImy = "/api/v1/populationstats";
+        var APIext="proxyEmigration";
         
-        $http.get(API)
+        $http.get(APIext)
             .then(function(response){
-                var data=response.data;
-                console.log(data);
+                
+                //récupération données extérieures
+                var dataext = response.data;
+                
+                $http.get(APImy)
+                    .then(function(response){
+                        
+                        //récupération de mes données
+                        var mydata = response.data;
+                        
+                        var mydataYear = mydata.map(function(item){
+                            return item.year;
+                        });
+                        
+                        var dataextYear = dataext.map(function(item){
+                            return item.year;
+                        });
+                        
+                        var myGermany = mydata.filter(function(item){
+                            if (item.country=='Germany')
+                                return(item);
+                        });
+                        
+                        var extGermany = dataext.filter(function(item){
+                            if(item.country=='Germany')
+                                return(item);
+                        });
+                        console.log(myGermany[1].year+' '+myGermany[1].totalpopulation);
+                        
+                        var graphdef = {
+                        	categories : ['Total Population','Total Emigration'],
+                        	dataset : {
+                        		'Total Population' : [
+                        			{ "name": myGermany[0].year, "value" : myGermany[0].totalpopulation },
+                        			{ "name": myGermany[1].year, "value" : myGermany[1].totalpopulation }
+                        		],
+                        		
+                        		'Total Emigration' : [
+                        			{ name : extGermany[0].year, value : extGermany[0].totalemigrant }
+                        		]
+                        	}
+                        };
+               
+                        var chart = uv.chart('Bar', graphdef,{
+                                	meta : {
+                                		caption : 'Germany',
+                                		subcaption : 'Total population and emigration over the years'
+	                               }});
+                        
+                    });
+                
                 
             });
             
