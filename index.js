472f48bcb82fd1fb37c4f140bb1574cdd0a2b1e8
sos1819gw <gauthier.wiemann@insa-rouen.fr>
2019-06-01 15:28:41 +0000

    organization of the integrations

--- index.js
+++ index.js
@@ -26,30 +26,6 @@ var REST_Countriesv1_API = require("./RESTCountriesV1");
 var GWRESTV1;
 REST_Countriesv1_API(app,GWRESTV1);
 
-//Access to G03-Companies
-var G03CompaniesAPI = 'https://sos1819-03.herokuapp.com/api/v1/companies';
-
-app.use('/proxyG03Companies', function(req, res) {
-  console.log('piped: '+G03CompaniesAPI);
-  req.pipe(request(G03CompaniesAPI)).pipe(res);
-});
-
-//Access to G10-biofuelsproduction
-var G10BiofuelsAPI = 'https://sos1819-10.herokuapp.com/api/v1/biofuels-production';
-
-app.use('/proxyG10Biofuels', function(req, res) {
-  console.log('piped: '+G10BiofuelsAPI);
-  req.pipe(request(G10BiofuelsAPI)).pipe(res);
-});
-
-//Access to G06-transfer-stats
-var G06TransferAPI = 'https://sos1819-06.herokuapp.com/api/v1/transfer-stats/';
-
-app.use('/proxyG06Transfer', function(req, res) {
-  console.log('piped: '+G06TransferAPI);
-  req.pipe(request(G06TransferAPI)).pipe(res);
-});
-
 //Access to G02-scorers-stats
 var G02ScorersAPI = 'https://sos1819-02.herokuapp.com/api/v1/scorers-stats';
 
@@ -58,12 +34,12 @@ app.use('/proxyG02Scorers', function(req, res) {
   req.pipe(request(G02ScorersAPI)).pipe(res);
 });
 
-//Access to G14-deceaseds
-var G14DeceasedsAPI = 'https://sos1819-14.herokuapp.com/api/v1/deceaseds/';
+//Access to G03-Companies
+var G03CompaniesAPI = 'https://sos1819-03.herokuapp.com/api/v1/companies';
 
-app.use('/proxyG14Deceaseds', function(req, res) {
-  console.log('piped: '+G14DeceasedsAPI);
-  req.pipe(request(G14DeceasedsAPI)).pipe(res);
+app.use('/proxyG03Companies', function(req, res) {
+  console.log('piped: '+G03CompaniesAPI);
+  req.pipe(request(G03CompaniesAPI)).pipe(res);
 });
 
 //Access to G04-beer-consumed-stats
@@ -74,6 +50,14 @@ app.use('/proxyG04Beer', function(req, res) {
   req.pipe(request(G04BeerAPI)).pipe(res);
 });
 
+//Access to G06-transfer-stats
+var G06TransferAPI = 'https://sos1819-06.herokuapp.com/api/v1/transfer-stats/';
+
+app.use('/proxyG06Transfer', function(req, res) {
+  console.log('piped: '+G06TransferAPI);
+  req.pipe(request(G06TransferAPI)).pipe(res);
+});
+
 //Access to G08-tourists-by-countries
 var G08TouristAPI = 'https://sos1819-08.herokuapp.com/api/v1/tourists-by-countries';
 
@@ -82,7 +66,16 @@ app.use('/proxyG08Tourist', function(req, res) {
   req.pipe(request(G08TouristAPI)).pipe(res);
 });
 
-//Access to G08-tourists-by-countries
+
+//Access to G10-biofuelsproduction
+var G10BiofuelsAPI = 'https://sos1819-10.herokuapp.com/api/v1/biofuels-production';
+
+app.use('/proxyG10Biofuels', function(req, res) {
+  console.log('piped: '+G10BiofuelsAPI);
+  req.pipe(request(G10BiofuelsAPI)).pipe(res);
+});
+
+//Access to G11-public-expenditure-educations
 var G11API = 'https://sos1819-11.herokuapp.com/api/v2/public-expenditure-educations/';
 
 app.use('/proxyG11', function(req, res) {
@@ -90,6 +83,13 @@ app.use('/proxyG11', function(req, res) {
   req.pipe(request(G11API)).pipe(res);
 });
 
+//Access to G14-deceaseds
+var G14DeceasedsAPI = 'https://sos1819-14.herokuapp.com/api/v1/deceaseds/';
+
+app.use('/proxyG14Deceaseds', function(req, res) {
+  console.log('piped: '+G14DeceasedsAPI);
+  req.pipe(request(G14DeceasedsAPI)).pipe(res);
+});
 
 // MongoDb
 var climates_stats_api = require("./climate-stats-api");
