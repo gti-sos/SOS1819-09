30d1a78a66d610b3405f057189d7d6b1679e40dc
sos1819gp <pimpinella.giuseppe@libero.it>
2019-06-02 20:22:47 +0000

    highchart terminated

--- public/integrations/jsGiuseppe/GPG02-ctrl-Movie.js
+++ public/integrations/jsGiuseppe/GPG02-ctrl-Movie.js
@@ -10,115 +10,147 @@ angular
             console.log("Data received " + JSON.stringify(response.data, null, 2));
             var DataMovie = response.data;
             
-            var DataNomination = [];
-            var DataMovieAward = [];
-            var DataMovieEdition = [];
-            
+            var arrcountry = [];
             var j = 0;
+            
             for (var i = 0; i<DataMovie.length; i++)
             {
-                if (DataMovie[i].country == "EEUU")
+                if (i==0) { arrcountry[j] = DataMovie[i].country; j++; }
+                else
                 {
-                    DataNomination[j] = [ DataMovie[i].year, DataMovie[i].movienomination ];
-                    DataMovieAward[j] = [ DataMovie[i].year, DataMovie[i].movieaward ];
-                    DataMovieEdition[j] = [ DataMovie[i].year, DataMovie[i].movieedition] ;
-                    j++;
+                    var found = false;
+                    for (var k = arrcountry.length-1; k>=0; k--)
+                        if (arrcountry[k] == DataMovie[i].country) found = true;
+                    if (!found) {arrcountry[j] = DataMovie[i].country; j++}
                 }
             }
-            
-            var arrayDataNom = []; 
-            var arrayDataMA = [];
-            var arrayDataME = [];
-            j = 0;
-            
-            for (var i = 1939; i<=1997; i++, j++)
+
+            for (var idschema = 0; idschema<arrcountry.length; idschema++)
             {
-                var found = false;
-                for (var k = 0; k<DataNomination.length; k++)
+            
+                var DataNomination = [];
+                var DataMovieAward = [];
+                var DataMovieEdition = [];
+                var filmlist = "";
+                
+                j = 0;
+                for (var i = 0; i<DataMovie.length; i++)
                 {
-                    var vetta = DataNomination[k];
-                    var vettb = DataMovieAward[k];
-                    var vettc = DataMovieEdition[k];
-                    var date = vetta[0];
-                    if (date == i) 
-                    { 
-                        arrayDataNom[j] = vetta[1]; 
-                        arrayDataMA[j] = vettb[1];
-                        arrayDataME[j] = vettc[1];
-                        found = true; 
-                        break; 
-                    }
-                }
-                if (!found)
-                {
-                    arrayDataNom[j] = null; 
-                    arrayDataMA[j] = null;
-                    arrayDataME[j] = null;
-                }
-            }
-            
-            
-            Highcharts.chart('container', {
-            	chart: {
-                  type: 'line'
-                },
-                title: {
-                    text: 'Premios a las mejores películas de Estados Unidos, desde 1939 hasta 1997'
-                },
-            
-                subtitle: {
-                    text: 'Fuente de Information: Alejandro Martin Mancheño'
-                },
-            
-                yAxis: {
-                    title: {
-                        text: 'Numero de premios'
-                    }
-                },
-                legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'middle'
-                },
-            
-                plotOptions: {
-                    series: {
-                        connectNulls: true,
-                        label: {
-                            connectorAllowed: false
-                        },
-                        pointStart: 1939
-                    }
-                },
-            
-                series: [{
-                    name: 'Nominaciones',
-                    data: arrayDataNom
-                }, {
-                    name: 'Movie Award',
-                    data: arrayDataMA
-                }, {
-                    name: 'Movie Edition',
-                    data: arrayDataME
-                }],
-            
-                responsive: {
-                    rules: [{
-                        condition: {
-                            maxWidth: 500
-                        },
-                        chartOptions: {
-                            legend: {
-                                layout: 'horizontal',
-                                align: 'center',
-                                verticalAlign: 'bottom'
-                            }
+                    if (DataMovie[i].country == arrcountry[idschema])
+                    {
+                        DataNomination[j] = [ DataMovie[i].year, DataMovie[i].movienomination ];
+                        DataMovieAward[j] = [ DataMovie[i].year, DataMovie[i].movieaward ];
+                        DataMovieEdition[j] = [ DataMovie[i].year, DataMovie[i].movieedition] ;
+                        if (filmlist == "") filmlist = DataMovie[i].name; else filmlist += DataMovie[i].name;
+                        var found = false;
+                        for (var k=i+1; k<DataMovie.length; k++)
+                        {
+                            if (DataMovie[k].country == arrcountry[idschema]) found = true;
                         }
-                    }]
+                        if (found == true) filmlist += ", "; else filmlist+=".";
+                        j++;
+                        
+                        
+                    }
                 }
-            
-            });
-            
+                
+                var arrayDataNom = []; 
+                var arrayDataMA = [];
+                var arrayDataME = [];
+                j = 0;
+                
+                for (var i = 1939; i<=2011; i++, j++)
+                {
+                    var found = false;
+                    for (var k = 0; k<DataNomination.length; k++)
+                    {
+                        var vetta = DataNomination[k];
+                        var vettb = DataMovieAward[k];
+                        var vettc = DataMovieEdition[k];
+                        var date = vetta[0];
+                        if (date == i) 
+                        { 
+                            arrayDataNom[j] = vetta[1]; 
+                            arrayDataMA[j] = vettb[1];
+                            arrayDataME[j] = vettc[1];
+                            found = true; 
+                            break; 
+                        }
+                    }
+                    if (!found)
+                    {
+                        arrayDataNom[j] = null; 
+                        arrayDataMA[j] = null;
+                        arrayDataME[j] = null;
+                    }
+                }
+                
+                var node = document.createElement("div");
+                var namenode = "node"+idschema;
+                node.setAttribute("id", namenode);
+                document.body.appendChild(node);
+                Highcharts.chart(namenode, {
+                	chart: {
+                      type: 'line'
+                    },
+                    title: {
+                        text: 'Premios a las mejores películas de Estados Unidos, desde 1939 hasta 1997, de los films ' + filmlist
+                    },
+                
+                    subtitle: {
+                        text: 'Fuente de Information: Alejandro Martin Mancheño'
+                    },
+                
+                    yAxis: {
+                        title: {
+                            text: 'Numero de premios'
+                        }
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle'
+                    },
+                
+                    plotOptions: {
+                        series: {
+                            connectNulls: true,
+                            label: {
+                                connectorAllowed: false
+                            },
+                            pointStart: 1939
+                        }
+                    },
+                
+                    series: [{
+                        name: 'Nominaciones',
+                        data: arrayDataNom
+                    }, {
+                        name: 'Movie Award',
+                        data: arrayDataMA
+                    }, {
+                        name: 'Movie Edition',
+                        data: arrayDataME
+                    }],
+                
+                    responsive: {
+                        rules: [{
+                            condition: {
+                                maxWidth: 500
+                            },
+                            chartOptions: {
+                                legend: {
+                                    layout: 'horizontal',
+                                    align: 'center',
+                                    verticalAlign: 'bottom'
+                                }
+                            }
+                        }]
+                    }
+                
+                });
+            }
+
         }, 
         function (error){});
 }]);
