3bfd6ce7982bf70db51ca6280923ddc233155789
sos1819gp <pimpinella.giuseppe@libero.it>
2019-06-03 19:44:30 +0000

    fix last things

--- public/integrations/jsGiuseppe/GPG14-ctrl-Elements.js
+++ public/integrations/jsGiuseppe/GPG14-ctrl-Elements.js
@@ -1,4 +1,4 @@
-/* global angular */
+/* global angular Chartkick*/
 
 angular
     .module("ProjectApp")
@@ -7,8 +7,27 @@ angular
         var API = "/proxyElements";
 
         $http.get(API).then(function(response){
-            console.log("Data received " + JSON.stringify(response.data, null, 2));
-            $scope.datas = response.data;
+            
+            var arrElements = response.data;
+            var finalarray = []; var j = 0;
+            for (var i=0; i<arrElements.length; i++)
+            {
+                finalarray[j] = [ "Las victimas en el año " + arrElements[i].year + " de " + arrElements[i].province, parseInt(arrElements[i].victims, 10) ]; 
+                j++;
+            }
+            for (var i=0; i<arrElements.length; i++)
+            {
+                finalarray[j] = [ "Heridos no hospitalizados en accidentes en el año " + arrElements[i].year + " de " + arrElements[i].province, parseInt(arrElements[i].injurednothospitalizedinaccidents, 10) ];
+                j++;
+            }
+            
+            for (var i=0; i<arrElements.length; i++)
+            {
+                finalarray[j] = [ "Accidentes con víctimas en el año " + arrElements[i].year + " de " + arrElements[i].province, parseInt(arrElements[i].accidentswithvictims, 10) ];
+                j++;
+            }
+            new Chartkick.BarChart("chart-1", finalarray);
+            
         }, 
         function (error){});
 }]);
